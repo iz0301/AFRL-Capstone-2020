67adb49153b36a400669ca77205be932e0dee070
iz0301 <isaaczachmann@gmail.com>
2020-01-28 12:46:56 -0500

    Added support to create_sorted_images so that it can sort defect types

---
+++ surface-defects/pytorch/all_in_one_classifier.py
@@ -0,0 +1,128 @@
+import numpy as np
+from skimage import io
+from matplotlib.pyplot import imshow
+import matplotlib.pyplot as plt
+import torch
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+import os
+import time
+import torch.nn as nn
+from os import path
+from torch.autograd import Variable
+import custom_nn as c
+import torchvision.transforms.functional as TF
+from custom_nn import ConvolutionalNN
+from torchvision.datasets import ImageFolder
+import math
+from image_segmentation import SegmentedImage
+import gc
+import sys
+from show_defects import show_defects
+
+#### If a GPU is available its probably faster so use that. If it isnt go ahead and use CPU
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+if device == 'cpu':
+    print('WARNING: Not using GPU. Consider using CUDA for possibly faster results')
+#device = 'cpu'
+
+
+### Set up parameters
+# Directory with training images sorted as data_dir/defect and data_dir/no_defect
+data_dir = "/home/isaac/Python/pytorch/AFRL-Capstone-2020/surface-defects/Defects/paper/flash/sorted/train"
+num_epochs = 20
+batch_size = 25
+learning_rate = 0.0008
+# Use square images with IMSZ width and height
+IMSZ = 150
+do_test = True # If we want to test at the end
+
+# If we are testing, directory for testing data folders in same format as training data
+test_dir = "/home/isaac/Python/pytorch/AFRL-Capstone-2020/surface-defects/Defects/paper/flash/sorted/test"
+
+# if we are testing, this is a single large image to cut up and test on (like what we would do to find where defects are in a total image)
+test_img = "/home/isaac/Python/pytorch/AFRL-Capstone-2020/surface-defects/Defects/paper/flash/Isaac_2_crop.png"
+
+
+#### Load in the data and convert it to a grayscale tensor
+dataset = ImageFolder(data_dir,  transform = transforms.Compose([transforms.Grayscale(), transforms.ToTensor()]))
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=5)
+
+### Set up our model
+ # Number of channels for each convolutional layer, and length of list is how many convolutional layers
+conv = [10,30,90,180]
+conv_layers = np.empty([len(conv),7])
+for i in range(len(conv)):
+    # Set up convolutional layers and maxpool layers based on the list above and also use:
+    # kernel_size=5, stride=1, padding=0, max_pool_kernel=2, max_pool_stride=2, max_pool_padding=0
+    conv_layers[i] = [conv[i], 5, 1, 0, 2, 2, 0]
+
+### Set up a pre_filter that made defects easier to see visually
+pre_filter_weights = [[-5, -10, -5], [-10, 60, -10], [-5, -10, -5]];
+pre_filter_weights = torch.FloatTensor(pre_filter_weights)
+pre_filter_weights = pre_filter_weights.repeat(1, 1, 1, 1)
+pre_filter = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, bias=False)
+pre_filter.weight.data = pre_filter_weights
+pre_filter.weight.requires_grad = True # Let the filter weights change during training
+
+# Create the convolutional nural network using the class in custom_nn.py
+# Use 3 fully connected layers and 1 output layer. Fully connected connected
+# layers have 1000, 100, then 50 nodes. Output layer has 1 node
+# Use random rectified linear unit as the activation function (see doc)
+cnn = ConvolutionalNN([IMSZ, IMSZ, 1], conv_layers, np.asarray([1000,100,50]), 3, nn.RReLU)
+cnn.init_weights(nn.init.calculate_gain) # Initialize wieghts
+ # Add the pre filter + the convolutional nural network + sigmoid activation function to get the full model
+ # Sigmoid activation makes output between -1 and 1
+ # (maybe we should change labels to be -1 and 1 then)
+model = nn.Sequential(pre_filter, cnn, nn.Sigmoid())
+
+
+
+# Print number of trainable model paraemters for debuging purposes
+model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+params_e = sum([np.prod(p.size()) for p in model_parameters])
+print('Network has ' + str(params_e))
+
+# Put the model on the gpu/cpu
+model = model.to(device)
+# Using the Adam optimizer
+optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate,weight_decay=0.00001)
+
+# Using binary cross entropy loss function
+lossFunc = nn.CrossEntropyLoss().to(device)
+
+#### Main training loop
+losses = []
+for epoch in range(num_epochs):
+    end_time = time.time() # Keeping track of time if we want to show a progress bar or soemthing later
+    totalCorrect = 0
+    num_guess_none = 0
+    # loop through the data
+    for batch_num, (img, target) in enumerate(dataloader):
+        start_time = time.time()
+
+        # Put the data onto gpu/cpu
+        img = Variable(img).to(device)
+        target = Variable(target).to(device)
+        # ===================forward=====================
+        output = model(img) # Get output
+        loss = lossFunc(output, target.float()) # calculate loss
+        # ===================backward====================
+        optimizer.zero_grad() # Clear old gradients
+        loss.backward() # Calculate new gradients
+        optimizer.step() # Update weights based on gradients
+
+        # Calculate how many were predicted correctly
+        #predicted = torch.round(torch.transpose(output,1,0))
+        _, predicted = torch.max(output, 1)
+        batchCorrect = (predicted == target).sum()
+
+        # Keep running total of correct
+        num_guess_none = num_guess_none + (predicted == 1).sum()
+        totalCorrect = totalCorrect + batchCorrect
+
+    end_time = time.time()
+
+    # Print training accuracy each epoch end
+    print("Training accuracy: " + str(round(totalCorrect.cpu().numpy()/len(dataset)*10000)/100))
+    losses.append(loss.detach().cpu().numpy())
--- surface-defects/pytorch/create_sorted_images.py
+++ surface-defects/pytorch/create_sorted_images.py
@@ -15,28 +15,35 @@ import matplotlib.pyplot as plt
 # These transforms could all be done while training, however they take a long
 # time so doing them before hand allows more model types to be tried faster with
 # The same datasets
+# Updating to sort based on type of defect as well
 
 # How many images to generate and what size should they be
 num_imgs = 100
 img_size = [150, 150]
+# Defects are looked for in this order so last should be no defect
+# 4th number is 1 for alpha values
+defect_colors = [[1,0,0,1],[0,1,0,1],[1,1,1,1]]
+defect_names = ["eraser","pencil","none"]
+no_defect_index = 2
 
 # Path pointing to the folder that contains the large image and the defect map of the image
 # The defect map should be pure black where the defects are and should be named IMG_NAME_defects.png
-path = "/home/isaac/Python/pytorch/AFRL-Capstone-2020/surface-defects/Defects/paper/flash/"
+path = "/Users/isaaczachmann/Desktop/imgs/flash"
 img = "Isaac_2_crop" # Without extension
 
 # Where to save the output images
-out_dir = "/home/isaac/Python/pytorch/AFRL-Capstone-2020/surface-defects/Defects/paper/flash/sorted/test"
+out_dir = "/Users/isaaczachmann/Desktop/imgs/flash/test"
 
 # Load the original image and the defect map
 og_img = io.imread(path + os.path.sep + img + ".png")
 og_defect = io.imread(path + os.path.sep + img + "_defects.png")
 
 # Make directories to save the output images
-os.makedirs(out_dir + "/defect/", exist_ok=True)
-os.makedirs(out_dir + "/no_defect/", exist_ok=True)
+for dn in defect_names:
+    os.makedirs(out_dir + os.sep + dn, exist_ok=True)
 
-num_defects = 0 # Running total of how many images we created had defects
+# Running total of how many of each type of defect
+num_defects = [0] * len(defect_names)
 
 # Loop to make all the images
 for i in range(num_imgs): # I keeps track of image number so we can save them with unique names
@@ -47,7 +54,7 @@ for i in range(num_imgs): # I keeps track of image number so we can save them wi
     # tries is the running counter of how many times we tried
     tries = 0
 
-    defective = False # Keep track of if the image has a defect
+    defect_type = -1 # Keep track of if the image has a defect
     # Keep track of if the image was clipped during rotation
     # if the image was clipped it cannot be used
     clipped = False
@@ -56,7 +63,7 @@ for i in range(num_imgs): # I keeps track of image number so we can save them wi
     # This is so we can get an even dataset even if defects do not appear
     # as 50 percent of the input image
     # Also we cant use a clipped image so if its clipped we keep looking
-    while (not defective and tries < num_tries) or clipped:
+    while ((defect_type == no_defect_index or defect_type == -1) and tries < num_tries) or clipped:
         img = og_img.copy() # Copy the original image so we can modify our copy
         defect = og_defect.copy() # Copy the defect map so we can modify our copy
 
@@ -65,13 +72,11 @@ for i in range(num_imgs): # I keeps track of image number so we can save them wi
         img = TF.to_pil_image(img)
         img = TF.to_grayscale(img)
         defect = TF.to_pil_image(defect)
-        defect = TF.to_grayscale(defect)
-
 
         # Set up other random rotation and apply to apply to both
         angle = random.randint(-180, 180)
         img = TF.rotate(img, angle)
-        defect = TF.rotate(defect, angle) # Cant use fill argument for TF.rotate()? pytorch too old?
+        defect = TF.rotate(defect, angle)  # Cant use fill argument for TF.rotate()? pytorch too old?
 
         # Set up random croping and apply to both
         h = img_size[0]
@@ -86,19 +91,21 @@ for i in range(num_imgs): # I keeps track of image number so we can save them wi
         if random.random() > 0.5:
             img = TF.hflip(img)
             defect = TF.hflip(defect)
-
         # Random vertical flipping (50% chance) and flip both
         if random.random() > 0.5:
             img = TF.vflip(img)
             defect = TF.vflip(defect)
 
-
-        # See if we have any defect in the defect map
-        min_pix = defect.getextrema()[0] # Get the darkest pixel
-        if(min_pix == 0): # If we have defects the dark pixel will be pure black
-            defective = True
-        else:
-            defective = False
+        # See what type of defect we have
+        for j, c in enumerate(defect_colors):
+            # See how many pixels we match all 3 channels to the test color
+            # For now just match at least 10
+            if torch.sum(torch.sum(TF.to_tensor(defect).permute(2,1,0) == torch.tensor(c), 2) == 4) > 9:
+                defect_type = j
+                break
+        # Make sure we found a defect type
+        if defect_type == -1:
+            raise Exception("Error: Could not determine type for image", img)
 
         # If the input image contains a pure black it is (slmost certainly)
         # from being cliped during rotation, which is bad
@@ -115,17 +122,13 @@ for i in range(num_imgs): # I keeps track of image number so we can save them wi
 
     # Once we exit the while loop we are ready to save our image into the correct folder
     # SAVE AS PNG! jpg has losses and it looks bad when it is loaded again later
-    if defective: # If we have defects
-        # Save the transformed image in the defects folder
-        torchvision.utils.save_image(img, out_dir + os.sep + "defect" + os.sep + str(i) + ".png")
-        num_defects = num_defects + 1
-    else: # if we dont
-        # Save the transformed image in the no_defects folder
-        torchvision.utils.save_image(img, out_dir + os.sep + "no_defect" + os.sep + str(i) + ".png")
+    torchvision.utils.save_image(img, out_dir + os.sep + defect_names[defect_type] + os.sep + str(i) + ".png")
+    num_defects[defect_type] = num_defects[defect_type] + 1
 
     # Print our progress
-    print(str(round(i/num_imgs*100,2)) + "%", end='\r')
+    print(str(round(i/num_imgs*100, 2)) + "%", end='\r')
 
 # Print our final percentage of how many images had defects and how many didnt
 # This way we can see if we our close to a 50/50 split that we want
-print("Genearated " + str(num_imgs) + " images, " + str(num_defects) + " (" + str(num_defects/num_imgs*100) + "%) had defects.")
+for k, t in enumerate(defect_names):
+    print(f"Generated {num_defects[k]} images with {t} defect ({100*num_defects[k]/num_imgs}%)")
